748b0dc3eca2fb1785702e795f77fb6f8306df00
KIAN <KIAN@DESKTOP-7E42V4K>
2021-11-29 17:31:47 +0330

    Add project files.

---
+++ 1s/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace mostatil
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("ars: ");
+			string ars = Console.ReadLine();
+			Console.Write("toul: ");
+			string toul = Console.ReadLine();
+			float mohit = (float.Parse(ars) + float.Parse(toul)) * 2;
+			float masohat = float.Parse(ars) * float.Parse(toul);
+			Console.WriteLine("mohit: " + mohit);
+			Console.WriteLine("masohat: " + masohat);
+			Console.ReadKey();
+		}
+	}
+}
